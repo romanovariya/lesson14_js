f4ddbe31d8154ffeab51a552dbf7df1a81b4329a
Riya Romanova <riyaromanova13@gmail.com>
2020-12-10 18:16:54 +0300

    fix(script.js): add cssText and fixed repeated code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function DomElement(selector, height, width, bg, fontSize) {
+function DomElement(selector = '#anyOne', height = '100px', width = '250px', bg = 'tomato', fontSize = '25px') {
     this.selector = selector;
     this.height = height;
     this.width = width;
@@ -11,29 +11,27 @@ function DomElement(selector, height, width, bg, fontSize) {
 DomElement.prototype.create = function(){
     let elem;
     let elemSelector;
+    elem = (this.selector[0] === '.') ? document.createElement('div') : document.createElement('p');
+    let str = this.selector;
+    elemSelector = str.slice(1, str.length + 1);
+    elem.className = elemSelector;
+    
     if(this.selector[0] === '.'){
-        elem = document.createElement('div');
-        let str = this.selector;
-        elemSelector = str.slice(1, str.length + 1);
         elem.className = elemSelector;
         elem.innerHTML = `Это блок с классом ${elemSelector}`;
-        document.body.append(elem);
     } else if(this.selector[0] === '#'){
-        elem = document.createElement('p');
-        let str = this.selector;
-        elemSelector = str.slice(1, str.length + 1);
         elem.id = elemSelector;
         elem.innerHTML = `Это параграф с id ${elemSelector}`;
-        document.body.append(elem);
     }
+    document.body.append(elem);
+    
     let getElem = document.querySelector(this.selector);
-    getElem.style.height = this.height;
-    getElem.style.width = this.width;
-    getElem.style.backgroundColor = this.bg;
-    getElem.style.fontSize = this.fontSize;
+    getElem.style.cssText = `height: ${this.height};
+        width: ${this.width};
+        background-color: ${this.bg};
+        font-size: ${this.fontSize};`;
 };
 
-let domElem1 = new DomElement('.classed', '200px', '200px', '#ffedb9', '11px');
+let domElem1 = new DomElement('.classed', '200px', '200px', '#ffedb9', '30px');
 
 domElem1.create();
-console.log(domElem1);
